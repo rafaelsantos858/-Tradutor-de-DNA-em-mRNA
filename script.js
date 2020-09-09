57f6efabecd9efe187d86c6aa3802204a75a1899
rafaelsantos858 <69479684+rafaelsantos858@users.noreply.github.com>
2020-09-09 16:44:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+$("#dna1").on('input', function () {
+    if (this.value == "A") {
+        $("#rna1").val("U")
+    } else if (this.value == "C") {
+        $("#rna1").val("G")
+    } else if (this.value == "G") {
+        $("#rna1").val("C")
+    } else if (this.value == "T") {
+        $("#rna1").val("A")
+    }
+})
+
+$("#dna2").on('input', function () {
+    if (this.value == "A") {
+        $("#rna2").val("U")
+    } else if (this.value == "C") {
+        $("#rna2").val("G")
+    } else if (this.value == "G") {
+        $("#rna2").val("C")
+    } else if (this.value == "T") {
+        $("#rna2").val("A")
+    }
+})
+
+$("#dna3").on('input', function () {
+    if (this.value == "A") {
+        $("#rna3").val("U")
+    } else if (this.value == "C") {
+        $("#rna3").val("G")
+    } else if (this.value == "G") {
+        $("#rna3").val("C")
+    } else if (this.value == "T") {
+        $("#rna3").val("A")
+    }
+})
+
+$("#dna4").on('input', function () {
+    if (this.value == "A") {
+        $("#rna4").val("U")
+    } else if (this.value == "C") {
+        $("#rna4").val("G")
+    } else if (this.value == "G") {
+        $("#rna4").val("C")
+    } else if (this.value == "T") {
+        $("#rna4").val("A")
+    }
+})
+
+
+
+// $('#btn').click(function () {
+    
+// var teste =  $('#rna1').val 
+
+// alert(teste)
+
+// })
